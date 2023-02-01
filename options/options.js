bed4dc23da20f42461517f2df2fb78cc1cb283e5
Crowdbotics <team@crowdbotics.com>
2023-02-01 13:43:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-bread-38631",
+  url: "https://silent-bread-38631.botics.co",
+  api: "https://silent-bread-38631.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
